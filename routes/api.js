160f070f99f3de4602c08b1dfa63eef065895551
Tim O'Malley <tomalley24@gmail.com>
2021-06-04 13:38:02 -0500

    fixed api/workouts/range route and added /api/workouts route

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,24 @@ const router = require("express").Router();
 const db = require("../models");
 
 
-router.get('/api/workouts', async ())
+router.get('/api/workouts', async (req, res) =>{
+    let data = await db.Workout.find({}).lean();
+    res.json(data);
+})
 
 router.get('/api/workouts/range', async (req, res) => {
-    let results = await db.Workout.find({}).lean();
-    res.json(results);
+    let data = await db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {$sum: '$exercises.duration'},
+            },
+        },
+    ]);
+    if(data.length > 7){
+        res.json(data.slice(data.length-7, data.length));
+    }else {
+        res.json(data);
+    }
 })
 
 module.exports = router;
